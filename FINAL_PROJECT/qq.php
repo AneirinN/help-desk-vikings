d7ef9032eef5a8d325f45de81421cc85a5bc7086
AneirinN <annunn@csumb.edu>
2015-05-01 00:54:16 -0700

    stash

--- FINAL_PROJECT/qq.php
+++ FINAL_PROJECT/qq.php
@@ -5,3 +5,25 @@
  * Date: 5/1/2015
  * Time: 12:51 AM
  */
+
+
+
+<?php
+$servername = "54.213.207.93";
+$username = "root";
+$password = "csumb-hdv";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+}
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
+
+
+
