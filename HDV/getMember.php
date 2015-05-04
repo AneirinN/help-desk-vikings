6d8b5a689fb5a917e2cb9c91b3d429b6745e9d68
AneirinN <annunn@csumb.edu>
2015-05-04 01:19:12 -0700

    modified the login process - renamed files, added logout, etc

---
+++ HDV/getMember.php
@@ -0,0 +1,38 @@
+<form id="form1" name="form1" method="post" action="post">
+<label for="memberID">Memberid</label><input type="text" name="memberID" id="memberID" />
+<br class="clear" /> 
+<label for="firstName">Firstname</label><input type="text" name="firstName" id="firstName" />
+<br class="clear" /> 
+<label for="lastName">Lastname</label><input type="text" name="lastName" id="lastName" />
+<br class="clear" /> 
+<label for="gender">Gender</label><input type="text" name="gender" id="gender" />
+<br class="clear" /> 
+<label for="birthday">Birthday</label><input type="text" name="birthday" id="birthday" />
+<br class="clear" /> 
+<label for="email">Email</label><input type="text" name="email" id="email" />
+<br class="clear" /> 
+<label for="address">Address</label><input type="text" name="address" id="address" />
+<br class="clear" /> 
+<label for="zip">Zip</label><input type="text" name="zip" id="zip" />
+<br class="clear" /> 
+<label for="city">City</label><input type="text" name="city" id="city" />
+<br class="clear" /> 
+<label for="state">State</label><input type="text" name="state" id="state" />
+<br class="clear" /> 
+<label for="phone1">Phone1</label><input type="text" name="phone1" id="phone1" />
+<br class="clear" /> 
+<label for="phone2">Phone2</label><input type="text" name="phone2" id="phone2" />
+<br class="clear" /> 
+<label for="schoolD">Schoold</label><input type="text" name="schoolD" id="schoolD" />
+<br class="clear" /> 
+<label for="skill">Skill</label><input type="text" name="skill" id="skill" />
+<br class="clear" /> 
+<label for="clubs">Clubs</label><input type="text" name="clubs" id="clubs" />
+<br class="clear" /> 
+<label for="hand">Hand</label><input type="text" name="hand" id="hand" />
+<br class="clear" /> 
+<label for="height">Height</label><input type="text" name="height" id="height" />
+<br class="clear" /> 
+<label for="contactID">Contactid</label><input type="text" name="contactID" id="contactID" />
+<br class="clear" /> 
+</form>
